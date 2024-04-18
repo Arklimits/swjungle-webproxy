5895b9e6d324b022cab71b94cab65e66f0830c0b
Arklimits <zaqokm2@gmail.com>
2024-04-18 15:55:16 +0000

    Fix typo

--- tiny/tiny.c
+++ tiny/tiny.c
@@ -105,9 +105,9 @@ void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longms
 void read_requesthdrs(rio_t *rp) {
     char buf[MAXLINE];
 
-    Rio_readlinb(rp, buf, MAXLINE);
+    Rio_readlineb(rp, buf, MAXLINE);
     while (strcmp(buf, "\r\n")) {
-        Rio_readlinb(rp, buf, MAXLINE);
+        Rio_readlineb(rp, buf, MAXLINE);
         printf("%s", buf);
     }
     return;
@@ -163,7 +163,7 @@ void serve_static(int fd, char *filename, int filesize) {
  *get_filetype - Derive file type from filename
  */
 void get_filetype(char *filename, char *filetype) {
-    if (strtsr(filename, ".html"))
+    if (strstr(filename, ".html"))
         strcpy(filetype, "text/html");
     else if (strstr(filename, ".gif"))
         strcpy(filetype, "image/gif");
