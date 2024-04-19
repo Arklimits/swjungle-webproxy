7e22e84377b5c6c45233009e437e15c6db0dfa15
Arklimits <zaqokm2@gmail.com>
2024-04-19 17:16:55 +0000

    Update main

--- proxy.c
+++ proxy.c
@@ -9,7 +9,29 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-int main() {
-  printf("%s", user_agent_hdr);
-  return 0;
+int main(int argc, char **argv) {
+    int listenfd, connfd;
+    char hostname[MAXLINE], port[MAXLINE];
+    socklen_t clientlen;
+    struct sockaddr_storage clientaddr;
+
+    /* Check command line args */
+    if (argc != 2) {
+        fprintf(stderr, "usage: %s <port>\n", argv[0]);
+        exit(1);
+    }
+
+    /* 지정된 포트로 소켓 열고 대기 */
+    listenfd = Open_listenfd(argv[1]);
+    while (1) {
+        clientlen = sizeof(clientaddr);
+        connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);                        // line:netp:tiny:accept 클라이언트 연결 수락
+        Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);  // 클라이언트 호스트명 및 포트 정보 수집
+        printf("Accepted connection from (%s, %s)\n", hostname, port);
+        proxy(connfd);   // line:netp:tiny:doit 클라이언트 요청 수행
+        Close(connfd);  // line:netp:tiny:close 통신 종료
+    }
+    printf("%s", user_agent_hdr);
+
+    return 0;
 }
