fa7f58fccb25c8a59def6f68c8d08b624303d26b
Arklimits <zaqokm2@gmail.com>
2024-04-20 06:16:48 +0000

    Update doit

--- proxy.c
+++ proxy.c
@@ -28,10 +28,52 @@ int main(int argc, char **argv) {
         connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);                        // line:netp:tiny:accept 클라이언트 연결 수락
         Getnameinfo((SA *)&clientaddr, clientlen, hostname, MAXLINE, port, MAXLINE, 0);  // 클라이언트 호스트명 및 포트 정보 수집
         printf("Accepted connection from (%s, %s)\n", hostname, port);
-        proxy(connfd);   // line:netp:tiny:doit 클라이언트 요청 수행
+        doit(connfd);   // line:netp:tiny:doit 클라이언트 요청 수행
         Close(connfd);  // line:netp:tiny:close 통신 종료
     }
     printf("%s", user_agent_hdr);
 
     return 0;
+}
+
+void doit(int fd) {  // fd: 클라이언트 연결을 나타내는 file descriptor
+    int is_static;
+    struct stat sbuf;
+    char buf[MAXLINE], method[MAXLINE], uri[MAXLINE], version[MAXLINE];
+    char filename[MAXLINE], cgiargs[MAXLINE];
+    rio_t rio;  // Remote I/O
+
+    /* Read request line and headers */
+    Rio_readinitb(&rio, fd);            // rio에 file descriptor 저장
+    Rio_readlineb(&rio, buf, MAXLINE);  // rio에 buffer 저장
+    printf("Reqeust headers:\n");
+    printf("%s", buf);
+    sscanf(buf, "%s %s %s", method, uri, version);
+
+    if (strcasecmp(method, "GET") && strcasecmp(method, "HEAD")) {  // GET / HEAD 요청 외에는 구현이 안돼있음
+        clienterror(fd, method, "501", "Not implemented", "Tiny does not implement this method");
+        return;
+    }
+    read_requesthdrs(&rio);
+
+    /* Parse URI from GET request */
+    is_static = parse_uri(uri, filename, cgiargs);  // URI 파싱
+    if (stat(filename, &sbuf) < 0) {                // sbuf에 filename을 꽂을 때 이상한 값이 들어오면
+        clienterror(fd, filename, "404", "Not found", "Tiny couldn't find this file");
+        return;
+    }
+
+    if (is_static) {                                                /* Serve static contenct */
+        if (!S_ISREG(sbuf.st_mode) || !(S_IRUSR & sbuf.st_mode)) {  // 권한이 없음
+            clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't read the file");
+            return;
+        }
+        serve_static(fd, filename, sbuf.st_size, method);
+    } else {                                                        /* Serve dynaic content */
+        if (!S_ISREG(sbuf.st_mode) || !(S_IXUSR & sbuf.st_mode)) {  // 권한이 없음
+            clienterror(fd, filename, "403", "Forbidden", "Tiny couldn't run the CGI program");
+            return;
+        }
+        serve_dynamic(fd, filename, cgiargs, method);
+    }
 }
