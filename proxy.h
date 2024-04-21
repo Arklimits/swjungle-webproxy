0d166a3fa373670e8b3b1b5547a7fabf73843ce9
Arklimits <zaqokm2@gmail.com>
2024-04-21 11:55:32 +0000

    Update cache entry

--- proxy.h
+++ proxy.h
@@ -18,10 +18,14 @@ void parse_uri(char *uri, char *host, char *path, char *port);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
 void *thread(void *vargp);
 
-typedef struct cache_entry{
+typedef struct cache_entry {
     char *path;
+    char *content_type;
+    int content_length;
+    void *content;
 
-}cache_entry;
+    struct cache_entry *prev, *next;
+} cache_entry;
 
 /*
  * print_log - 로그 파일 작성을 위한 함수
