2731ce2c4ca738953024ab5845ecffd01ca3f39d
Arklimits <zaqokm2@gmail.com>
2024-04-21 12:37:59 +0000

    Update cache_init

--- proxy.h
+++ proxy.h
@@ -9,27 +9,42 @@ static const char *user_agent_hdr =
     "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 "
     "Firefox/10.0.3\r\n";
 
-/* Print log fucntion for test */
+/* Print log function for test */
 const void print_log(char *desc, char *text);
 
+/* Thread Function */
+void *thread(void *vargp);
+
+/* Cache Function */
+typedef struct cache_t cache_t;
+typedef struct cache_list cache_list;
+const void cache_init(cache_list *list);
+const void cache_insert(cache_list *list, cache_t *ptr);
+const void cache_find(cache_t *ptr);
+const void cache_delete(cache_list *list, cache_t *ptr);
+const void cache_purge(cache_t *ptr);
+
+/* Proxy.c Functions */
 void doit(int fd);
 void write_requesthdrs(char *buf, char *method, char *path, char *version, char *host, char *port);
 void parse_uri(char *uri, char *host, char *path, char *port);
 void clienterror(int fd, char *cause, char *errnum, char *shortmsg, char *longmsg);
-void *thread(void *vargp);
-
-typedef struct cache_t cache_t;
-typedef struct cache_list cache_list;
 
 struct cache_t {
     char *key[MAXBUF];
     char *val[MAX_OBJECT_SIZE];
-    cache_t *next;
+    cache_t *prev, *next;
 };
 
 struct cache_list {
-    cache_list *head, *tail;
+    cache_t *head, *tail;
 };
+
+const void cache_init(cache_list *list){
+    list = (cache_list*)calloc(1, sizeof(cache_list));
+    list->head = NULL;
+    list->tail = NULL;
+}
 
 /*
  * print_log - 로그 파일 작성을 위한 함수
